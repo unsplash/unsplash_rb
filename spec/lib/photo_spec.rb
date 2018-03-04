1f234f2e6a47a5f8f97aeea2a525b0143d2f9e08
Gustavo Sobral <ghsobral@gmail.com>
2018-03-04 19:19:08 +0100

    Allow Photo#search to recieve orientation parameter

--- spec/lib/photo_spec.rb
+++ spec/lib/photo_spec.rb
@@ -119,6 +119,10 @@ describe Unsplash::Photo do
   end
 
   describe "#search" do
+    before do
+      Unsplash::Client.connection = Unsplash::Connection.new(api_base_uri: "https://api.unsplash.com/")
+    end
+
     it "returns a SearchResult of Photos" do
       VCR.use_cassette("photos") do
         @photos = Unsplash::Photo.search("dog", 1)
@@ -126,8 +130,8 @@ describe Unsplash::Photo do
 
       expect(@photos).to be_an Unsplash::SearchResult
       expect(@photos.size).to eq 10
-      expect(@photos.total).to eq 541
-      expect(@photos.total_pages).to eq 55
+      expect(@photos.total).to eq 1001
+      expect(@photos.total_pages).to eq 101
     end
 
     it "returns a SearchResult of Photos with number of elements per page defined" do
@@ -137,8 +141,17 @@ describe Unsplash::Photo do
 
       expect(@photos).to be_an Unsplash::SearchResult
       expect(@photos.size).to eq 3
-      expect(@photos.total).to eq 541
-      expect(@photos.total_pages).to eq 181
+      expect(@photos.total).to eq 1001
+      expect(@photos.total_pages).to eq 334
+    end
+
+    it "returns a SearchResult of Photos with orientation parameter" do
+      VCR.use_cassette("photos") do
+        @photos = Unsplash::Photo.search("dog", 1, 3, :portrait)
+      end
+
+      expect(@photos).to be_an Unsplash::SearchResult
+      expect(@photos.size).to eq 3
     end
   end
 
