bcb5c9d603265b11c4b5ff21a166b103b6c0a9c0
Aaron Klaassen <aaron@unsplash.com>
2019-03-19 18:14:15 -0500

    Remove Collection#curated.

--- lib/unsplash/collection.rb
+++ lib/unsplash/collection.rb
@@ -8,9 +8,8 @@ module Unsplash # :nodoc:
       # Get a specific collection.
       # @param id [Integer] The ID of the collection.
       # @return [Unsplash::Collection] The requested collection.
-      def find(id, curated = false)
-        url = ["/collections", (curated ? "curated" : nil), id].compact.join("/")
-        Unsplash::Collection.new JSON.parse(connection.get(url).body)
+      def find(id)
+        Unsplash::Collection.new JSON.parse(connection.get("/collections/#{id}").body)
       end
 
       # Get a list of all collections.
@@ -37,19 +36,6 @@ module Unsplash # :nodoc:
           per_page: per_page
         }
         list = JSON.parse(connection.get("/collections/featured", params).body)
-        list.map { |data| Unsplash::Collection.new(data) }
-      end
-
-      # Get a list of all curated collections.
-      # @param page [Integer] Which page of search results to return.
-      # @param per_page [Integer] The number of search results per page. (default: 10, maximum: 30)
-      # @return [Array] A single page of the +Unsplash::Collection+ list.
-      def curated(page = 1, per_page = 10)
-        params = {
-          page:     page,
-          per_page: per_page
-        }
-        list = JSON.parse(connection.get("/collections/curated", params).body)
         list.map { |data| Unsplash::Collection.new(data) }
       end
 
